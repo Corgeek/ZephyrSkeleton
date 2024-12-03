c58f0f4a6402bcfb8e65b16e4f3cf7fa0a7b5d57
Takayuki Goto <tkg.develop@gmail.com>
2024-12-04 01:09:03 +0900

    organize CMakeLists.txt

---
+++ boards/bbc/microbit/bbc_microbit.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Takayuki Goto <tkg.develop@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+
+#include "drivers/button/drv_button_microbit.h"
+#include "drivers/led/display/drv_disp_microbit.h"
+#include "drivers/sensor/accel/drv_accel_lis2dh.h"
+#include "drivers/sensor/magnet/drv_magn_lis2mdl.h"
+
+void board_init(void)
+{
+	drv_init_button();
+	drv_init_display();
+	drv_init_accel();
+	drv_init_magnet();
+}
